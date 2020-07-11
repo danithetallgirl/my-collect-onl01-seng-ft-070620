aa3cce053b39eb55e374dddedfb7dcd533060515
Dani Bassett <dani@danithetallgirl.com>
2020-07-11 03:05:51 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,7 @@
  def my_collect(array)
    empty_array = []
     counter = 0
-      my_collect(empty_array) << yield
+      my_collect(empty_array) << yield(array[i])
         counter += 1
       end
   end
