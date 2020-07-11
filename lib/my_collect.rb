db7928b392f830d29af476431018867a46044a33
Dani Bassett <dani@danithetallgirl.com>
2020-07-11 03:10:44 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,33 +1,14 @@
 def my_collect(array)
-  empty_array = []
-  counter = 0
-  my_collect(empty_array) << yield(array[i])
-  counter += 1
-  end
-
-end
-
-def hello(array)
   i = 0
-  collection = []
+  collect = []
   while i < array.length
-    collection << yield(array[i])
+    collect << yield(array[i])
     i += 1
   end
-  collection
+  collect
 end
 
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
-  name.split(" ").first
-end
-
-def hello(array)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << yield(array[i])
-    i += 1
-  end
-  collection
-end
+#array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+#my_collect(array) do |name|
+ # name.split(" ").first
+#end
